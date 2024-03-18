790e5e13e069cc5b6979818620f0f79f273cce06
JShantz2022 <josslyneshantz@gmail.com>
2024-03-17 21:36:32 -0400

    Added Reviews Methods
    Added methods for reviews. Debugging will happed with pages creation.

---
+++ MediLink/Models/PractitionerReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MediLink.Entities;
+
+namespace MediLink.Models
+{
+    public class PractitionerReviewViewModel
+    {
+        public PractitionerReview review = new PractitionerReview();
+
+        public List<PractitionerReview> currentPractitionerReviews { get; set; }
+
+        public List<PractitionerReview> pastPractitionerReviews { get; set; }
+
+        public List<PractitionerPatient> currentPractitioners { get; set; }
+
+        public List<PractitionerPatient> pastPractitioners { get; set; }
+    }
+}
